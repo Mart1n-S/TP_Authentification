814751db4b4ae81c72fe3eef6d7a296ae30d71cb
Martin <martinsimongo@gmail.com>
2024-11-02 14:47:54 +0100

    Mise en place de l'inscription + connexion

---
+++ assets/controllers/inscription_controller.js
@@ -0,0 +1,173 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = [
+    "input",
+    "error",
+    "criteria",
+    "length",
+    "uppercase",
+    "lowercase",
+    "number",
+    "special",
+    "toggleIcon",
+  ];
+
+  connect() {
+    // Affiche les erreurs existantes au chargement de la page
+    this.errorTargets.forEach((error) => {
+      if (error.textContent.trim() !== "") {
+        error.classList.remove("d-none");
+      }
+    });
+
+    // Gère l'affichage des critères de mot de passe
+    const passwordInput = this.inputTargets.find(
+      (input) => input.name === "registration_form[plainPassword][first]"
+    );
+    const confirmPasswordInput = this.inputTargets.find(
+      (input) => input.name === "registration_form[plainPassword][second]"
+    );
+
+    if (passwordInput) {
+      passwordInput.addEventListener("focus", () => {
+        this.criteriaTarget.classList.remove("d-none");
+      });
+      passwordInput.addEventListener("blur", () => {
+        if (passwordInput.value === "") {
+          this.criteriaTarget.classList.add("d-none");
+        }
+      });
+      passwordInput.addEventListener("input", (event) =>
+        this.validatePasswordCriteria(event)
+      );
+    }
+
+    if (confirmPasswordInput) {
+      confirmPasswordInput.addEventListener("input", (event) =>
+        this.validatePasswordMatch(event)
+      );
+    }
+  }
+
+  validate(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    const errorMessage = this.getErrorMessage(name, value);
+    const label = target.labels[0];
+    if (errorMessage || errorMessage === "") {
+      this.showError(name, errorMessage);
+      this.setInvalid(target, label);
+    } else {
+      this.hideError(name);
+      this.setValid(target, label);
+    }
+
+    // Appelle la validation de correspondance des mots de passe
+    if (name === "registration_form[plainPassword][second]") {
+      this.validatePasswordMatch(event);
+    }
+  }
+
+  validatePasswordCriteria(event) {
+    const password = event.target.value;
+
+    // Validation des critères individuels
+    this.toggleCriteria(this.lengthTarget, password.length >= 16);
+    this.toggleCriteria(this.uppercaseTarget, /[A-Z]/.test(password));
+    this.toggleCriteria(this.lowercaseTarget, /[a-z]/.test(password));
+    this.toggleCriteria(this.numberTarget, /[0-9]/.test(password));
+    this.toggleCriteria(this.specialTarget, /[\W_]/.test(password));
+  }
+
+  validatePasswordMatch(event) {
+    const password = this.inputTargets.find(
+      (input) => input.name === "registration_form[plainPassword][first]"
+    ).value;
+    const confirmPassword = event.target.value;
+    const errorMessage = "Les mots de passe doivent correspondre.";
+    if (password !== confirmPassword) {
+      this.showError("registration_form[plainPassword][second]", errorMessage);
+      this.setInvalid(event.target, event.target.labels[0]);
+    } else {
+      this.hideError("registration_form[plainPassword][second]");
+      this.setValid(event.target, event.target.labels[0]);
+    }
+  }
+
+  toggleCriteria(target, isValid) {
+    target.classList.toggle("text-success", isValid);
+    target.classList.toggle("text-danger", !isValid);
+  }
+
+  getErrorMessage(name, value) {
+    switch (name) {
+      case "registration_form[name]":
+        const nameRegex = /^[A-Za-zÀ-ÿ\s-]{2,50}$/;
+        return !nameRegex.test(value) ? "Le nom n'est pas valide." : null;
+      case "registration_form[email]":
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+        return !emailRegex.test(value) ? "L'email n'est pas valide." : null;
+      case "registration_form[plainPassword][first]":
+        const passwordRegex =
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{16,}$/;
+        return !passwordRegex.test(value) ? "" : null;
+      case "registration_form[agreeTerms]":
+        return !this.inputTargets[4].checked
+          ? "Veuillez accepter les CGU."
+          : null;
+      default:
+        return null;
+    }
+  }
+
+  showError(name, message) {
+    const index = this.inputTargets.findIndex((input) => input.name === name);
+    if (index >= 0) {
+      this.errorTargets[index].textContent = message;
+      this.errorTargets[index].classList.remove("d-none");
+    }
+  }
+
+  hideError(name) {
+    const index = this.inputTargets.findIndex((input) => input.name === name);
+    if (index >= 0) {
+      this.errorTargets[index].classList.add("d-none");
+      this.errorTargets[index].textContent = "";
+    }
+  }
+
+  setValid(target, label) {
+    target.classList.add("is-valid");
+    label.classList.add("text-success");
+    target.classList.remove("is-invalid");
+    label.classList.remove("text-danger");
+  }
+
+  setInvalid(target, label) {
+    target.classList.add("is-invalid");
+    label.classList.add("text-danger");
+    target.classList.remove("is-valid");
+    label.classList.remove("text-success");
+  }
+
+  togglePasswordVisibility() {
+    // Recherche tous les inputs de type password parmi les inputTargets
+    const passwordFields = this.inputTargets.filter((input) =>
+      input.classList.contains("password")
+    );
+    const icon = this.toggleIconTarget;
+
+    // Alterne entre "text" et "password" pour chaque champ de mot de passe
+    passwordFields.forEach((passwordField) => {
+      if (passwordField.type === "password") {
+        passwordField.type = "text";
+        icon.classList.replace("bi-eye", "bi-eye-slash");
+      } else {
+        passwordField.type = "password";
+        icon.classList.replace("bi-eye-slash", "bi-eye");
+      }
+    });
+  }
+}
---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Nom',
+                    'minlength' => 2,
+                    'maxlength' => 50,
+                    'pattern' => '^[A-Za-zÀ-ÿ\s-]+$',
+                    'title' => 'Veuillez entrer un nom valide.',
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le nom est obligatoire.',
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'minMessage' => 'Votre nom doit comporter au moins {{ limit }} caractères.',
+                        'max' => 50,
+                        'maxMessage' => 'Votre nom ne peut pas dépasser {{ limit }} caractères.',
+                    ]),
+                    new Regex([
+                        'pattern' => '/^[A-Za-zÀ-ÿ\s-]+$/',
+                        'message' => 'Le nom doit commencer par une lettre et ne doit contenir que des lettres, des accents et des traits d’union.',
+                    ]),
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Email',
+                    'maxlength' => 180,
+                    'pattern' => '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
+                    'title' => 'Veuillez entrer un email valide.',
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'L\'email est obligatoire.',
+                    ]),
+                    new Email([
+                        'message' => 'Veuillez entrer un email valide.',
+                    ]),
+                    new Length([
+                        'max' => 180,
+                        'maxMessage' => 'L\'adresse email ne peut pas dépasser {{ limit }} caractères.',
+                    ]),
+                    new Regex([
+                        'pattern' => '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/',
+                        'message' => 'Veuillez entrer une adresse email valide.',
+                    ]),
+                ],
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'label' => 'Accepter les CGU',
+                'label_attr' => [
+                    'class' => 'form-check-label',
+                ],
+                'mapped' => false,
+                'attr' => [
+                    'class' => 'form-check-input',
+                ],
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'Vous devez accepter les conditions.',
+                    ]),
+                ],
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passe doivent correspondre.',
+                'first_options'  => [
+                    'label' => 'Mot de passe',
+                    'attr' => [
+                        'class' => 'form-control password',
+                        'placeholder' => 'Mot de passe',
+                        'autocomplete' => 'new-password',
+                        'minlength' => 16,
+                        'maxlength' => 255,
+                        'pattern' => '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{16,255}$',
+                        'title' => 'Le mot de passe doit contenir au moins 16 caractères, dont une lettre majuscule, une lettre minuscule, un chiffre, et un caractère spécial.',
+                    ],
+                ],
+                'second_options' => [
+                    'label' => 'Confirmation du mot de passe',
+                    'attr' => [
+                        'class' => 'form-control password',
+                        'placeholder' => 'Confirmer le mot de passe',
+                        'autocomplete' => 'new-password',
+                    ],
+                ],
+                'mapped' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le mot de passe est obligatoire.',
+                    ]),
+                    new Length([
+                        'min' => 16,
+                        'minMessage' => 'Votre mot de passe doit comporter au moins {{ limit }} caractères.',
+                        'max' => 255,
+                    ]),
+                    new Regex([
+                        'pattern' => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{16,255}$/',
+                        'message' => 'Le mot de passe doit contenir au moins une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial.',
+                    ]),
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
